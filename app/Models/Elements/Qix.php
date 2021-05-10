ac572e763152224d8720aa77ea8d21d8c7e4e6ea
Artūrs Vīksne <artursviksne04@gmail.com>
2021-05-10 14:15:42 +0300

    Unit tests step_2

---
+++ app/Models/Elements/Qix.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Models\Elements;
+
+
+use App\Models\Input;
+
+class Qix implements ElementInterface
+{
+
+    public function name(): ?string
+    {
+        // TODO: Implement name() method.
+    }
+
+    public function handle(Input $input): void
+    {
+        // TODO: Implement handle() method.
+    }
+}
