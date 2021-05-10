6980dc285022053c9eab9d607f2749cdde8e3289
Artūrs Vīksne <artursviksne04@gmail.com>
2021-05-10 14:27:49 +0300

    Unit tests step_2

--- test/Unit/AppTest.php
+++ test/Unit/AppTest.php
@@ -8,6 +8,7 @@ use App\Models\App;
 use App\Models\Elements\Bar;
 use App\Models\Elements\ElementCollection;
 use App\Models\Elements\Foo;
+use App\Models\Elements\Qix;
 use App\Models\Input;
 use PHPUnit\Framework\TestCase;
 
@@ -19,17 +20,22 @@ class AppTest extends TestCase
         $elements->addMany([
             new Foo(),
             new Bar(),
+            new Qix()
         ]);
         $app = new App($elements);
 
         $inputFoo = new Input('3');
         $inputBar = new Input('5');
+        $inputQix = new Input('7');
         $inputFooBar = new Input('15');
+        $inputFooBarQix = new Input('105');
         $input = new Input('2');
 
         self::assertEquals('Foo', $app->execute($inputFoo));
         self::assertEquals('Bar', $app->execute($inputBar));
+        self::assertEquals('Qix', $app->execute($inputQix));
         self::assertEquals('FooBar', $app->execute($inputFooBar));
+        self::assertEquals('FooBarQix', $app->execute($inputFooBarQix));
         self::assertEquals('2', $app->execute($input));
     }
 }
